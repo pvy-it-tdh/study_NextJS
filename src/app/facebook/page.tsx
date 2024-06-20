b3b507d263b85898c6ab0608ab16b38b2c584a26
pvy-it-tdh <phucvy107@gmail.com>
2024-06-20 20:55:50 +0700

    add useRouter

--- src/app/facebook/page.tsx
+++ src/app/facebook/page.tsx
@@ -1,8 +1,16 @@
+'use client'
+import { useRouter } from "next/navigation";
+
 const Facebook=()=>{
+    const router= useRouter();
+    const handleClick=()=>{
+        router.push('/')
+    }
     return (
-        <>
+        <div>
             Facebook
-        </>
+            <button onClick={handleClick}>Back</button>
+        </div>
     )
 }
 export default Facebook;
